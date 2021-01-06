82d3e4b7291805f97835cb3154c5ad54c90f0f41
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2021-01-06 23:01:29 +0100

    add GraphQL error handling

--- web-react/src/index.js
+++ web-react/src/index.js
@@ -3,10 +3,38 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  concat,
+} from '@apollo/client'
+//https://harsh-patel.medium.com/handle-and-inspect-network-errors-in-your-graphql-client-side-ae5e4fafc08
+//https://hasura.io/blog/handling-graphql-hasura-errors-with-react/
+
+import { onError } from '@apollo/client/link/error'
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_URI || '/graphql',
+})
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  console.error(`[GraphQL error]: Operation: ${operation.operationName}`)
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations }) => {
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
+          locations
+        )}`
+      )
+    })
+
+  if (networkError) console.error(`[Network error]: ${networkError}`)
+})
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URI || '/graphql',
+  link: concat(errorLink, httpLink),
   cache: new InMemoryCache(),
 })
 
