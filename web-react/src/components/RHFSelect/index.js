4ae98e6d01e641501fa3075b3c4db821225e4911
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2021-01-17 17:15:57 +0100

    update Select

--- web-react/src/components/RHFSelect/index.js
+++ web-react/src/components/RHFSelect/index.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import { FormControl, InputLabel, Select } from '@material-ui/core'
+import React, { useMemo } from 'react'
+import { FormControl, InputLabel, Select, Input } from '@material-ui/core'
 import { Controller } from 'react-hook-form'
 
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+}
 const ReactHookFormSelect = ({
   name,
   label,
@@ -10,16 +20,26 @@ const ReactHookFormSelect = ({
   children,
   ...props
 }) => {
-  const labelId = `${name}-label`
+  const labelId = useMemo(() => `${name}-label`, [])
   return (
     <FormControl {...props}>
       <InputLabel id={labelId}>{label}</InputLabel>
       <Controller
-        as={
-          <Select labelId={labelId} label={label}>
+        render={({ onChange, onBlur, value, name, ref }) => (
+          <Select
+            ref={ref}
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+            name={name}
+            labelId={labelId}
+            label={label}
+            input={<Input />}
+            MenuProps={MenuProps}
+          >
             {children}
           </Select>
-        }
+        )}
         name={name}
         control={control}
         defaultValue={defaultValue}
