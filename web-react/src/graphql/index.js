77571b79f25b9c1a1bce01688a95841fd48cd91b
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2021-02-10 18:00:13 +0100

    update graphql setup

--- web-react/src/graphql/index.js
+++ web-react/src/graphql/index.js
@@ -23,47 +23,62 @@ const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
   }
 })
 
+// const cacheRead = (existing, { args: { filter, offset = 0, first } }) => {
+//   // A read function should always return undefined if existing is
+//   // undefined. Returning undefined signals that the field is
+//   // missing from the cache, which instructs Apollo Client to
+//   // fetch its value from your GraphQL server.
+//   // console.log(
+//   //   'existing, filter, offset, first: ',
+//   //   existing,
+//   //   filter,
+//   //   offset,
+//   //   first
+//   // )
+//   if (filter && !Object.values(filter).every(o => o === '')) {
+//     return undefined
+//   }
+//   if (!filter && !offset && !first) {
+//     return undefined
+//   }
+//   return existing && existing.slice(offset, offset + first)
+// }
+
+// const cacheMerge = (existing, incoming, props) => {
+//   // Slicing is necessary because the existing data is
+//   // immutable, and frozen in development.
+//   // console.log('props: ', props)
+//   const merged = existing ? existing.slice(0) : []
+//   if (props.args) {
+//     for (let i = 0; i < incoming.length; ++i) {
+//       merged[props.args.offset + i] = incoming[i]
+//     }
+//   }
+//   return merged
+// }
+
 const cache = new InMemoryCache({
   typePolicies: {
     Player: {
       keyFields: ['playerId'],
     },
-    Query: {
-      fields: {
-        Player: {
-          read(existing, { args: { filter, offset, first } }) {
-            // A read function should always return undefined if existing is
-            // undefined. Returning undefined signals that the field is
-            // missing from the cache, which instructs Apollo Client to
-            // fetch its value from your GraphQL server.
-            // console.log(
-            //   'existing, filter, offset, first: ',
-            //   existing,
-            //   filter,
-            //   offset,
-            //   first
-            // )
-            if (filter && !Object.values(filter).every(o => o === '')) {
-              return undefined
-            }
-            if (!filter && !offset && !first) {
-              return undefined
-            }
-            return existing && existing.slice(offset, offset + first)
-          },
-          keyArgs: ['playerId'],
-          merge(existing, incoming, { args: { offset = 0 } }) {
-            // Slicing is necessary because the existing data is
-            // immutable, and frozen in development.
-            const merged = existing ? existing.slice(0) : []
-            for (let i = 0; i < incoming.length; ++i) {
-              merged[offset + i] = incoming[i]
-            }
-            return merged
-          },
-        },
-      },
+    Team: {
+      keyFields: ['teamId'],
     },
+    // Query: {
+    //   fields: {
+    //     Player: {
+    //       read: cacheRead,
+    //       keyArgs: ['playerId'],
+    //       merge: cacheMerge,
+    //     },
+    //     Team: {
+    //       read: cacheRead,
+    //       keyArgs: ['teamId'],
+    //       merge: cacheMerge,
+    //     },
+    //   },
+    // },
   },
 })
 
