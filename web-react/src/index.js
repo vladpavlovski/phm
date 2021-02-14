3b277fc9fa42274d25ba779d099d36f0a165a11a
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2021-02-14 12:27:09 +0100

    add Bugfender

--- web-react/src/index.js
+++ web-react/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloProvider } from '@apollo/client'
+import { Bugfender } from '@bugfender/sdk'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { client } from './graphql'
+
+Bugfender.init({
+  appKey: process.env.REACT_APP_BUGFENDER_KEY,
+  // apiURL: 'https://api.bugfender.com',
+  // baseURL: 'https://dashboard.bugfender.com',
+  // overrideConsoleMethods: true,
+  // printToConsole: true,
+  // registerErrorHandler: true,
+  // logBrowserEvents: true,
+  // logUIEvents: true,
+  // version: '',
+  // build: '',
+})
 
 const Main = () => (
   <ApolloProvider client={client}>
