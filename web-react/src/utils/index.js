ed1e4ba27b317c64aa3132f15b63ade466fe402f
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2021-02-16 21:01:51 +0100

    update utils

--- web-react/src/utils/index.js
+++ web-react/src/utils/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import * as R from 'ramda'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -15,3 +16,15 @@ export const toTitleCase = R.compose(
 export const dateExist = date => date !== '0000-01-01'
 
 export const checkId = id => (id === 'new' ? uuidv4() : id)
+
+export const arrayToStringList = (data, keyId, keyValue = 'name') =>
+  data && (
+    <span>
+      {data.map((item, i) => (
+        <React.Fragment key={item[keyId]}>
+          <span>{`${item[keyValue]}`}</span>
+          {i !== data.length - 1 && ', '}
+        </React.Fragment>
+      ))}
+    </span>
+  )
