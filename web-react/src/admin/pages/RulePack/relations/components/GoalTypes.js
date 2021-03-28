ece2551964b4e5b38e4b4eebf7096774765a3a31
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2021-03-28 22:25:15 +0200

    RulePack - GoalTypes - GoalSubTypes

--- web-react/src/admin/pages/RulePack/relations/components/GoalTypes.js
+++ web-react/src/admin/pages/RulePack/relations/components/GoalTypes.js
@@ -33,7 +33,11 @@ import { RHFInput } from '../../../../../components/RHFInput'
 import { Loader } from '../../../../../components/Loader'
 import { Error } from '../../../../../components/Error'
 import { useStyles } from '../../../commonComponents/styled'
-import { setIdFromEntityId } from '../../../../../utils'
+import {
+  setIdFromEntityId,
+  checkId,
+  getXGridValueFromArray,
+} from '../../../../../utils'
 
 const GET_GOAL_TYPES = gql`
   query getRulePack($rulePackId: ID) {
@@ -44,6 +48,11 @@ const GET_GOAL_TYPES = gql`
         goalTypeId
         name
         code
+        subTypes {
+          goalSubTypeId
+          name
+          code
+        }
       }
     }
   }
@@ -80,6 +89,45 @@ const DELETE_GOAL_TYPE = gql`
   mutation deleteGoalType($goalTypeId: ID!) {
     deleted: DeleteGoalType(goalTypeId: $goalTypeId) {
       goalTypeId
+    }
+  }
+`
+
+const MERGE_GOAL_TYPE_GOAL_SUB_TYPE = gql`
+  mutation mergeRulePackGoalSubType(
+    $goalTypeId: ID!
+    $goalSubTypeId: ID!
+    $name: String
+    $code: String
+  ) {
+    goalSubType: MergeGoalSubType(
+      goalSubTypeId: $goalSubTypeId
+      name: $name
+      code: $code
+    ) {
+      goalSubTypeId
+      name
+    }
+    goalSubTypeGoalType: MergeGoalSubTypeGoalType(
+      from: { goalTypeId: $goalTypeId }
+      to: { goalSubTypeId: $goalSubTypeId }
+    ) {
+      from {
+        name
+      }
+      to {
+        goalSubTypeId
+        name
+        code
+      }
+    }
+  }
+`
+
+const DELETE_GOAL_SUB_TYPE = gql`
+  mutation deleteGoalSubType($goalSubTypeId: ID!) {
+    deleted: DeleteGoalSubType(goalSubTypeId: $goalSubTypeId) {
+      goalSubTypeId
     }
   }
 `
@@ -184,6 +232,14 @@ const GoalTypes = props => {
         width: 100,
       },
       {
+        field: 'subTypes',
+        headerName: 'Sub Types',
+        width: 300,
+        valueGetter: params => {
+          return getXGridValueFromArray(params.row.subTypes, 'name')
+        },
+      },
+      {
         field: 'goalTypeId',
         headerName: 'Edit',
         width: 120,
@@ -191,7 +247,7 @@ const GoalTypes = props => {
         renderCell: params => {
           return (
             <Button
-              onClick={() => handleOpenDialog(params.row)}
+              onClick={() => handleOpenDialog(params.value)}
               variant={'outlined'}
               size="small"
               className={classes.submit}
@@ -283,7 +339,9 @@ const GoalTypes = props => {
         rulePackId={rulePackId}
         openDialog={openDialog}
         handleCloseDialog={handleCloseDialog}
-        data={formData.current}
+        data={rulePack?.goalTypes?.find(
+          gt => gt.goalTypeId === formData.current
+        )}
       />
     </Accordion>
   )
@@ -291,7 +349,7 @@ const GoalTypes = props => {
 
 const FormDialog = props => {
   const { rulePack, rulePackId, openDialog, handleCloseDialog, data } = props
-
+  const [newSubType, setNewSubType] = useState(false)
   const classes = useStyles()
   const { enqueueSnackbar } = useSnackbar()
 
@@ -386,23 +444,26 @@ const FormDialog = props => {
       fullWidth
       maxWidth="md"
       open={openDialog}
-      onClose={handleCloseDialog}
+      onClose={() => {
+        handleCloseDialog()
+        setNewSubType(false)
+      }}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className={classes.form}
-        noValidate
-        autoComplete="off"
-      >
-        <DialogTitle id="alert-dialog-title">{`Add new goal type to ${rulePack?.name}`}</DialogTitle>
-        <DialogContent>
+      <DialogTitle id="alert-dialog-title">{`Add new goal type to ${rulePack?.name}`}</DialogTitle>
+      <DialogContent>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={classes.form}
+          noValidate
+          autoComplete="off"
+        >
           <Container>
             <Grid container spacing={2}>
               <Grid item xs={12} md={12} lg={12}>
                 <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6} md={6} lg={6}>
+                  <Grid item xs={12} sm={5} md={5} lg={5}>
                     <RHFInput
                       control={control}
                       defaultValue={data?.name || ''}
@@ -414,7 +475,7 @@ const FormDialog = props => {
                       error={errors?.name}
                     />
                   </Grid>
-                  <Grid item xs={12} sm={6} md={6} lg={6}>
+                  <Grid item xs={12} sm={5} md={5} lg={5}>
                     <RHFInput
                       control={control}
                       defaultValue={data?.code || ''}
@@ -430,23 +491,303 @@ const FormDialog = props => {
               </Grid>
             </Grid>
           </Container>
-        </DialogContent>
+        </form>
 
-        <DialogActions>
-          <Button
-            type="button"
-            onClick={() => {
-              handleCloseDialog()
-            }}
-          >
-            {'Cancel'}
-          </Button>
-          <LoadingButton type="submit" pending={loadingMergeGoalType}>
-            {loadingMergeGoalType ? 'Saving...' : 'Save'}
-          </LoadingButton>
-        </DialogActions>
-      </form>
+        <div style={{ margin: '2rem 0' }}>
+          {data?.subTypes?.map(st => (
+            <SubType
+              key={st.goalSubTypeId}
+              rulePack={rulePack}
+              rulePackId={rulePackId}
+              goalTypeId={data?.goalTypeId}
+              setNewSubType={setNewSubType}
+              data={st}
+            />
+          ))}
+        </div>
+        <div style={{ margin: '2rem 0' }}>
+          {data?.goalTypeId && newSubType ? (
+            <SubType
+              rulePack={rulePack}
+              rulePackId={rulePackId}
+              goalTypeId={data?.goalTypeId}
+              setNewSubType={setNewSubType}
+              data={{
+                goalSubTypeId: null,
+                name: '',
+                code: '',
+              }}
+            />
+          ) : (
+            <Button
+              type="button"
+              variant="contained"
+              onClick={() => {
+                setNewSubType(true)
+              }}
+            >
+              Add new subType
+            </Button>
+          )}
+        </div>
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          type="button"
+          onClick={() => {
+            handleCloseDialog()
+            setNewSubType(false)
+          }}
+        >
+          {'Cancel'}
+        </Button>
+        <LoadingButton
+          type="button"
+          onClick={handleSubmit(onSubmit)}
+          pending={loadingMergeGoalType}
+        >
+          {loadingMergeGoalType ? 'Saving...' : 'Save'}
+        </LoadingButton>
+      </DialogActions>
     </Dialog>
+  )
+}
+
+const SubType = props => {
+  const { rulePack, rulePackId, goalTypeId, data, setNewSubType } = props
+  const classes = useStyles()
+  const { enqueueSnackbar } = useSnackbar()
+
+  const { handleSubmit, control, errors } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const [
+    mergeGoalTypeGoalSubType,
+    { loading: loadingMergeGoalSubType },
+  ] = useMutation(MERGE_GOAL_TYPE_GOAL_SUB_TYPE, {
+    update(cache, { data: { goalSubTypeGoalType } }) {
+      try {
+        const queryResult = cache.readQuery({
+          query: GET_GOAL_TYPES,
+          variables: {
+            rulePackId,
+          },
+        })
+
+        const goalType = queryResult.rulePack[0].goalTypes.find(
+          gt => gt.goalTypeId === goalTypeId
+        )
+        const existingData = goalType.subTypes
+        const newItem = goalSubTypeGoalType.to
+
+        let updatedData = []
+        if (
+          existingData.find(ed => ed.goalSubTypeId === newItem.goalSubTypeId)
+        ) {
+          // replace if item exist in array
+          updatedData = existingData.map(ed =>
+            ed.goalSubTypeId === newItem.goalSubTypeId ? newItem : ed
+          )
+        } else {
+          // add new item if item not in array
+          updatedData = [...existingData, newItem]
+        }
+
+        const updatedResult = {
+          rulePack: [
+            {
+              ...queryResult.rulePack[0],
+              goalTypes: [
+                ...queryResult.rulePack[0].goalTypes.filter(
+                  gt => gt.goalTypeId !== goalTypeId
+                ),
+                { ...goalType, subTypes: updatedData },
+              ],
+            },
+          ],
+        }
+        cache.writeQuery({
+          query: GET_GOAL_TYPES,
+          data: updatedResult,
+          variables: {
+            rulePackId,
+          },
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    onCompleted: data => {
+      enqueueSnackbar(
+        `${data.goalSubTypeGoalType.to.name} added to ${rulePack.name}!`,
+        {
+          variant: 'success',
+        }
+      )
+      setNewSubType(false)
+    },
+    onError: error => {
+      enqueueSnackbar(`Error happened :( ${error}`, {
+        variant: 'error',
+      })
+      console.error(error)
+    },
+  })
+
+  const [
+    deleteGoalSubType,
+    { loading: mutationLoadingDeleteGoalSubType },
+  ] = useMutation(DELETE_GOAL_SUB_TYPE, {
+    variables: {
+      goalSubTypeId: data?.goalSubTypeId,
+    },
+    update(cache, { data: { deleted } }) {
+      try {
+        const queryResult = cache.readQuery({
+          query: GET_GOAL_TYPES,
+          variables: {
+            rulePackId,
+          },
+        })
+
+        const goalType = queryResult.rulePack[0].goalTypes.find(
+          gt => gt.goalTypeId === goalTypeId
+        )
+
+        const updatedData = goalType.subTypes.filter(
+          p => p.goalSubTypeId !== deleted.goalSubTypeId
+        )
+
+        const updatedResult = {
+          rulePack: [
+            {
+              ...queryResult.rulePack[0],
+              goalTypes: [
+                ...queryResult.rulePack[0].goalTypes.filter(
+                  gt => gt.goalTypeId !== goalTypeId
+                ),
+                {
+                  ...goalType,
+                  subTypes: updatedData,
+                },
+              ],
+            },
+          ],
+        }
+        cache.writeQuery({
+          query: GET_GOAL_TYPES,
+          data: updatedResult,
+          variables: {
+            rulePackId,
+          },
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    onCompleted: () => {
+      enqueueSnackbar(`GoalSubType was deleted!`, {
+        variant: 'info',
+      })
+    },
+    onError: error => {
+      enqueueSnackbar(`Error happened :( ${error}`, {
+        variant: 'error',
+      })
+      console.error(error)
+    },
+  })
+
+  const onSubmit = useCallback(
+    dataToCheck => {
+      try {
+        const { name, code } = dataToCheck
+
+        mergeGoalTypeGoalSubType({
+          variables: {
+            goalTypeId,
+            name,
+            code,
+            goalSubTypeId: checkId(data?.goalSubTypeId || 'new'),
+          },
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    [goalTypeId, data]
+  )
+
+  return (
+    <Container>
+      <form
+        onSubmit={null}
+        className={classes.form}
+        noValidate
+        autoComplete="off"
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={5} md={5} lg={5}>
+                <RHFInput
+                  control={control}
+                  defaultValue={data?.name || ''}
+                  name="name"
+                  label="SubType Name"
+                  required
+                  fullWidth
+                  variant="standard"
+                  error={errors?.name}
+                />
+              </Grid>
+              <Grid item xs={12} sm={5} md={5} lg={5}>
+                <RHFInput
+                  control={control}
+                  defaultValue={data?.code || ''}
+                  name="code"
+                  label="SubType Code"
+                  required
+                  fullWidth
+                  variant="standard"
+                  error={errors?.code}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={1} md={1} lg={1}>
+                {data?.goalSubTypeId && (
+                  <LoadingButton
+                    onClick={() => {
+                      deleteGoalSubType()
+                    }}
+                    type="button"
+                    pending={mutationLoadingDeleteGoalSubType}
+                  >
+                    {mutationLoadingDeleteGoalSubType
+                      ? 'Deleting...'
+                      : 'Delete'}
+                  </LoadingButton>
+                )}
+              </Grid>
+
+              <Grid item xs={12} sm={1} md={1} lg={1}>
+                <LoadingButton
+                  onClick={() => {
+                    handleSubmit(onSubmit)()
+                  }}
+                  type="button"
+                  pending={loadingMergeGoalSubType}
+                >
+                  {loadingMergeGoalSubType ? 'Saving...' : 'Save'}
+                </LoadingButton>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
   )
 }
 
