dc87d59527436375df2644d0f50eddb3ac1e39a0
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2021-04-17 14:11:54 +0200

    add RHFTimepicker component

---
+++ web-react/src/components/RHFTimepicker/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Controller } from 'react-hook-form'
+import TextField from '@material-ui/core/TextField'
+import dayjs from 'dayjs'
+import TimePicker from '@material-ui/lab/TimePicker'
+import { getDateFromTime } from '../../utils'
+
+const RHFTimepicker = props => {
+  const {
+    control,
+    name,
+    defaultValue,
+    variant,
+    error,
+    fullWidth,
+    ...rest
+  } = props
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ onChange, onBlur, value, name, ref }) => (
+        <TimePicker
+          {...rest}
+          renderInput={params => (
+            <TextField
+              {...params}
+              fullWidth={fullWidth}
+              variant={variant}
+              error={!!error}
+              helperText={!!error && error?.message}
+            />
+          )}
+          inputRef={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value}
+          name={name}
+        />
+      )}
+      defaultValue={defaultValue ? dayjs(getDateFromTime(defaultValue)) : null}
+    />
+  )
+}
+
+RHFTimepicker.defaultProps = {
+  defaultValue: null,
+  fullWidth: false,
+  openTo: 'hours',
+  disableFuture: false,
+  views: ['hours', 'minutes', 'seconds'],
+  error: false,
+  ampm: false,
+  ampmInClock: false,
+}
+
+export { RHFTimepicker }
