70abb84b527bf0dde57eb289ebff592be568a5ea
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2021-10-16 13:58:29 +0200

    RHFSwith memoization

--- web-react/src/components/RHFCheckbox/index.js
+++ web-react/src/components/RHFCheckbox/index.js
@@ -3,7 +3,7 @@ import { Controller } from 'react-hook-form'
 import PropTypes from 'prop-types'
 import { FormControlLabel, FormHelperText, Switch } from '@mui/material'
 
-const RHFSwitch = ({ label, error, name, id, ...props }) => (
+const RHFSwitchComponent = ({ label, error, name, id, ...props }) => (
   <>
     <FormControlLabel
       control={<Controller as={Switch} {...props} />}
@@ -15,10 +15,12 @@ const RHFSwitch = ({ label, error, name, id, ...props }) => (
   </>
 )
 
-RHFSwitch.propTypes = {
+RHFSwitchComponent.propTypes = {
   control: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
 }
+
+const RHFSwitch = React.memo(RHFSwitchComponent)
 
 export { RHFSwitch }
