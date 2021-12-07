5dbe24c0bfe0f743213f38be204bd3a0866f6d3b
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2021-12-07 22:57:52 +0100

    Update testing setup #47

---
+++ web-react/src/utils/test-utils.tsx
@@ -0,0 +1,33 @@
+import React, { FC, ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+
+import AdapterDayJs from '@mui/lab/AdapterDayjs'
+import { ThemeProvider } from '@mui/material/styles'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import { SnackbarProvider } from 'notistack'
+import { LayoutProvider } from 'context/layout/Provider'
+import { OrganizationProvider } from 'context/organization'
+
+import theme from 'styles/global'
+
+const AllTheProviders: FC = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDayJs}>
+        <SnackbarProvider maxSnack={5}>
+          <OrganizationProvider>
+            <LayoutProvider>{children}</LayoutProvider>
+          </OrganizationProvider>
+        </SnackbarProvider>
+      </LocalizationProvider>
+    </ThemeProvider>
+  )
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
