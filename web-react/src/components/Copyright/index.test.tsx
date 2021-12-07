5d40413fe8be88a8ec738c18a605644267caabf3
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2021-12-07 22:58:57 +0100

    Test: Copyright component

---
+++ web-react/src/components/Copyright/index.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'utils/test-utils'
+
+import { Copyright } from './index'
+
+describe('Copyright', () => {
+  test('renders Copyright component', () => {
+    render(<Copyright />)
+  })
+})
