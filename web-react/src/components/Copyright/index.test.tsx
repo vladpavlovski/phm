2b4b1c76409034fca7ce27607e561e2d02448397
Vlad Pavlovski <vladislav.pavlovski@gmail.com>
2021-12-08 13:21:49 +0100

    Test: Copyright component update #47

--- web-react/src/components/Copyright/index.test.tsx
+++ web-react/src/components/Copyright/index.test.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import { render } from 'utils/test-utils'
+import { unmountComponentAtNode } from 'react-dom'
+import { render, screen } from 'utils/test-utils'
 
 import { Copyright } from './index'
 
+let container: any = null
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
 describe('Copyright', () => {
-  test('renders Copyright component', () => {
+  it('renders Copyright component', async () => {
     render(<Copyright />)
+
+    expect(
+      await screen.findByText('HMS - Hockey Management System')
+    ).toBeInTheDocument()
   })
 })
