3da8eecc91ed33da1266621f8d1118a259b60154
Vlad Pavlovski <vladislav.pavlovski@ataccama.com>
2022-05-15 16:48:56 +0200

    Team. add icon to team color. update colorpicker

--- web-react/src/admin/pages/Team/index.tsx
+++ web-react/src/admin/pages/Team/index.tsx
@@ -19,6 +19,7 @@ import { decomposeDate, isValidUuid } from 'utils'
 import { Team as TeamType } from 'utils/types'
 import { gql, useApolloClient, useMutation, useQuery } from '@apollo/client'
 import { yupResolver } from '@hookform/resolvers/yup'
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
 import { Container, Grid, Paper } from '@mui/material'
 import MenuItem from '@mui/material/MenuItem'
 import Toolbar from '@mui/material/Toolbar'
@@ -325,235 +326,230 @@ const Team: React.FC = () => {
   return (
     <Container maxWidth="lg">
       {queryLoading && <Loader />}
-      {queryError && <Error message={queryError?.message} />}
-      {errorDelete && <Error message={errorDelete?.message} />}
-      {(mutationErrorMerge || mutationErrorCreate) && (
-        <Error
-          message={
-            mutationErrorMerge?.message || mutationErrorCreate?.message || ''
-          }
-        />
-      )}
-      {(teamData || teamId === 'new') &&
-        !queryLoading &&
-        !queryError &&
-        !mutationErrorMerge && (
-          <>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              noValidate
-              autoComplete="off"
-            >
-              <Helmet>
-                <title>{teamData.name || 'Team'}</title>
-              </Helmet>
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={4} lg={3}>
-                  <Paper className={classes.paper}>
-                    <Img
-                      placeholder={placeholderOrganization}
-                      src={teamData.logo}
-                      className={classes.logo}
-                      alt={teamData.name}
+      <Error
+        message={
+          mutationErrorMerge?.message ||
+          mutationErrorCreate?.message ||
+          errorDelete?.message ||
+          queryError?.message
+        }
+      />
+      {(teamData || teamId === 'new') && !queryLoading && (
+        <>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
+            <Helmet>
+              <title>{teamData.name || 'Team'}</title>
+            </Helmet>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={4} lg={3}>
+                <Paper className={classes.paper}>
+                  <Img
+                    placeholder={placeholderOrganization}
+                    src={teamData.logo}
+                    className={classes.logo}
+                    alt={teamData.name}
+                  />
+
+                  <RHFInput
+                    style={{ display: 'none' }}
+                    defaultValue={teamData.logo}
+                    control={control}
+                    name="logo"
+                    label="Logo URL"
+                    disabled
+                    fullWidth
+                    variant="standard"
+                    error={errors.logo}
+                  />
+
+                  {isValidUuid(teamId) && (
+                    <Uploader
+                      buttonText={'Change logo'}
+                      onSubmit={updateLogo}
+                      folderName="images/teams"
                     />
-
-                    <RHFInput
-                      style={{ display: 'none' }}
-                      defaultValue={teamData.logo}
-                      control={control}
-                      name="logo"
-                      label="Logo URL"
-                      disabled
-                      fullWidth
-                      variant="standard"
-                      error={errors.logo}
-                    />
-
-                    {isValidUuid(teamId) && (
-                      <Uploader
-                        buttonText={'Change logo'}
-                        onSubmit={updateLogo}
-                        folderName="images/teams"
-                      />
-                    )}
-                  </Paper>
-                </Grid>
-
-                <Grid item xs={12} md={12} lg={9}>
-                  <Paper className={classes.paper}>
-                    <Toolbar disableGutters className={classes.toolbarForm}>
-                      <div>
-                        <Title>{'Team'}</Title>
-                      </div>
-                      <div>
-                        {formState.isDirty && (
-                          <ButtonSave
-                            loading={
-                              mutationLoadingMerge || mutationLoadingCreate
-                            }
-                          />
-                        )}
-                        {teamId !== 'new' && (
-                          <ButtonDelete
-                            loading={loadingDelete}
-                            onClick={() => {
-                              deleteTeam()
-                            }}
-                          />
-                        )}
-                      </div>
-                    </Toolbar>
-
-                    <Grid container spacing={2}>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFInput
-                          defaultValue={teamData.name}
-                          control={control}
-                          name="name"
-                          label="Name"
-                          required
-                          fullWidth
-                          variant="standard"
-                          error={errors.name}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFInput
-                          defaultValue={teamData.fullName}
-                          control={control}
-                          name="fullName"
-                          label="Full name"
-                          fullWidth
-                          variant="standard"
-                          error={errors.fullName}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFInput
-                          defaultValue={teamData.nick}
-                          control={control}
-                          name="nick"
-                          label="Nick"
-                          fullWidth
-                          variant="standard"
-                          error={errors.nick}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFInput
-                          defaultValue={teamData.short}
-                          control={control}
-                          name="short"
-                          label="Short"
-                          fullWidth
-                          variant="standard"
-                          error={errors.short}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFSelect
-                          fullWidth
-                          control={control}
-                          name="status"
-                          label="Status"
-                          defaultValue={teamData?.status || ''}
-                          error={errors.status}
-                        >
-                          {activityStatusList.map(s => {
-                            return (
-                              <MenuItem key={s.value} value={s.value}>
-                                {s.name}
-                              </MenuItem>
-                            )
-                          })}
-                        </RHFSelect>
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFInput
-                          defaultValue={teamData.externalId}
-                          control={control}
-                          name="externalId"
-                          label="External Id"
-                          fullWidth
-                          variant="standard"
-                          error={errors.externalId}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFInput
-                          defaultValue={teamData.logo}
-                          control={control}
-                          name="logo"
-                          label="Logo URL"
-                          fullWidth
-                          variant="standard"
-                          error={errors.logo}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}></Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFColorpicker
-                          name="primaryColor"
-                          label="Primary Color"
-                          fullWidth
-                          variant="standard"
-                          control={control}
-                          defaultValue={teamData.primaryColor}
-                          error={errors.primaryColor}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFColorpicker
-                          name="secondaryColor"
-                          label="Secondary Color"
-                          fullWidth
-                          variant="standard"
-                          control={control}
-                          defaultValue={teamData.secondaryColor}
-                          error={errors.secondaryColor}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFColorpicker
-                          name="tertiaryColor"
-                          label="Tertiary Color"
-                          fullWidth
-                          variant="standard"
-                          control={control}
-                          defaultValue={teamData.tertiaryColor}
-                          error={errors.tertiaryColor}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={3} lg={3}>
-                        <RHFDatepicker
-                          fullWidth
-                          control={control}
-                          variant="standard"
-                          name="foundDate"
-                          label="Found Date"
-                          id="foundDate"
-                          openTo="year"
-                          disableFuture
-                          inputFormat={'DD/MM/YYYY'}
-                          views={['year', 'month', 'day']}
-                          defaultValue={teamData?.foundDate}
-                          error={errors?.foundDate}
-                        />
-                      </Grid>
-                    </Grid>
-                  </Paper>
-                </Grid>
+                  )}
+                </Paper>
               </Grid>
-            </form>
-            {isValidUuid(teamId) && (
-              <Relations
-                teamId={teamId}
-                team={teamData}
-                updateTeam={updateTeam}
-              />
-            )}
-          </>
-        )}
+
+              <Grid item xs={12} md={12} lg={9}>
+                <Paper className={classes.paper}>
+                  <Toolbar disableGutters className={classes.toolbarForm}>
+                    <div>
+                      <Title>{'Team'}</Title>
+                    </div>
+                    <div>
+                      {formState.isDirty && (
+                        <ButtonSave
+                          loading={
+                            mutationLoadingMerge || mutationLoadingCreate
+                          }
+                        />
+                      )}
+                      {teamId !== 'new' && (
+                        <ButtonDelete
+                          loading={loadingDelete}
+                          onClick={() => {
+                            deleteTeam()
+                          }}
+                        />
+                      )}
+                    </div>
+                  </Toolbar>
+
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFInput
+                        defaultValue={teamData.name}
+                        control={control}
+                        name="name"
+                        label="Name"
+                        required
+                        fullWidth
+                        variant="standard"
+                        error={errors.name}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFInput
+                        defaultValue={teamData.fullName}
+                        control={control}
+                        name="fullName"
+                        label="Full name"
+                        fullWidth
+                        variant="standard"
+                        error={errors.fullName}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFInput
+                        defaultValue={teamData.nick}
+                        control={control}
+                        name="nick"
+                        label="Nick"
+                        fullWidth
+                        variant="standard"
+                        error={errors.nick}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFInput
+                        defaultValue={teamData.short}
+                        control={control}
+                        name="short"
+                        label="Short"
+                        fullWidth
+                        variant="standard"
+                        error={errors.short}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFSelect
+                        fullWidth
+                        control={control}
+                        name="status"
+                        label="Status"
+                        defaultValue={teamData?.status || ''}
+                        error={errors.status}
+                      >
+                        {activityStatusList.map(s => {
+                          return (
+                            <MenuItem key={s.value} value={s.value}>
+                              {s.name}
+                            </MenuItem>
+                          )
+                        })}
+                      </RHFSelect>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFInput
+                        defaultValue={teamData.externalId}
+                        control={control}
+                        name="externalId"
+                        label="External Id"
+                        fullWidth
+                        variant="standard"
+                        error={errors.externalId}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFInput
+                        defaultValue={teamData.logo}
+                        control={control}
+                        name="logo"
+                        label="Logo URL"
+                        fullWidth
+                        variant="standard"
+                        error={errors.logo}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}></Grid>
+                    <Grid item>
+                      <RHFColorpicker
+                        name="primaryColor"
+                        label="Primary Color"
+                        fullWidth
+                        variant="standard"
+                        control={control}
+                        defaultValue={teamData.primaryColor}
+                        error={errors.primaryColor}
+                        iconEnd={<AccountBoxIcon />}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFColorpicker
+                        name="secondaryColor"
+                        label="Secondary Color"
+                        fullWidth
+                        variant="standard"
+                        control={control}
+                        defaultValue={teamData.secondaryColor}
+                        error={errors.secondaryColor}
+                        iconEnd={<AccountBoxIcon />}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFColorpicker
+                        name="tertiaryColor"
+                        label="Tertiary Color"
+                        fullWidth
+                        variant="standard"
+                        control={control}
+                        defaultValue={teamData.tertiaryColor}
+                        error={errors.tertiaryColor}
+                        iconEnd={<AccountBoxIcon />}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={3}>
+                      <RHFDatepicker
+                        fullWidth
+                        control={control}
+                        variant="standard"
+                        name="foundDate"
+                        label="Found Date"
+                        id="foundDate"
+                        openTo="year"
+                        disableFuture
+                        inputFormat={'DD/MM/YYYY'}
+                        views={['year', 'month', 'day']}
+                        defaultValue={teamData?.foundDate}
+                        error={errors?.foundDate}
+                      />
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+            </Grid>
+          </form>
+          {isValidUuid(teamId) && (
+            <Relations
+              teamId={teamId}
+              team={teamData}
+              updateTeam={updateTeam}
+            />
+          )}
+        </>
+      )}
     </Container>
   )
 }
--- web-react/src/components/RHFColorpicker/index.tsx
+++ web-react/src/components/RHFColorpicker/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { Controller, Control } from 'react-hook-form'
+import { Control, Controller } from 'react-hook-form'
+import InputAdornment from '@mui/material/InputAdornment'
 import TextField, { TextFieldProps } from '@mui/material/TextField'
-import { DEFAULT_CONVERTER, converters } from './transformers'
 import PickerDialog from './PickerDialog'
+import { converters, DEFAULT_CONVERTER } from './transformers'
 
 type IRHFColorpicker = TextFieldProps & {
   control: Control
@@ -11,53 +12,82 @@ type IRHFColorpicker = TextFieldProps & {
     message: string
   }
   defaultValue: string
+  iconStart?: React.ReactNode
+  iconEnd?: React.ReactNode
 }
 
 const RHFColorpicker: React.FC<IRHFColorpicker> = props => {
-  const { control, name, error, defaultValue, ...restProps } = props
+  const {
+    control,
+    name,
+    error,
+    defaultValue,
+    iconStart,
+    iconEnd,
+    ...restProps
+  } = props
 
   const [showPicker, setShowPicker] = useState(false)
   const [internalValue, setInternalValue] = useState(defaultValue || '')
   return (
-    <Controller
-      defaultValue={defaultValue || ''}
-      control={control}
-      name={name}
-      render={({ onChange, ...renderProps }) => (
-        <>
-          <TextField
-            {...restProps}
-            {...renderProps}
-            onClick={() => setShowPicker(true)}
-            onChange={e => {
-              setInternalValue(e.target.value)
-              onChange(e.target.value)
-            }}
-            error={!!error}
-            helperText={!!error && error.message}
-            InputProps={{
-              style: {
-                color: internalValue,
-              },
-            }}
-          />
-          {showPicker && (
-            <PickerDialog
-              value={internalValue}
-              onClick={() => {
-                setShowPicker(false)
-                onChange(internalValue)
+    <>
+      <Controller
+        defaultValue={defaultValue || ''}
+        control={control}
+        name={name}
+        render={({ onChange, ...renderProps }) => (
+          <>
+            <TextField
+              {...restProps}
+              {...renderProps}
+              onClick={() => setShowPicker(true)}
+              onChange={e => {
+                setInternalValue(e.target.value)
+                onChange(e.target.value)
               }}
-              onChange={c => {
-                const newValue = converters[DEFAULT_CONVERTER](c)
-                setInternalValue(newValue)
-                onChange(newValue)
+              error={!!error}
+              helperText={!!error && error.message}
+              InputProps={{
+                style: {
+                  color: internalValue,
+                },
+                startAdornment: iconStart ? (
+                  <InputAdornment position="start">
+                    {React.isValidElement(iconStart) &&
+                      React.cloneElement(iconStart, {
+                        style: { color: internalValue },
+                      })}
+                  </InputAdornment>
+                ) : null,
+                endAdornment: iconEnd ? (
+                  <InputAdornment position="end">
+                    {React.isValidElement(iconEnd) &&
+                      React.cloneElement(iconEnd, {
+                        style: { color: internalValue },
+                      })}
+                  </InputAdornment>
+                ) : null,
               }}
             />
-          )}
-        </>
-      )}
-    />
+
+            {showPicker && (
+              <PickerDialog
+                value={internalValue}
+                onClick={() => {
+                  setShowPicker(false)
+                  onChange(internalValue)
+                }}
+                onChange={c => {
+                  const newValue = converters[DEFAULT_CONVERTER](c)
+                  setInternalValue(newValue)
+                  onChange(newValue)
+                }}
+              />
+            )}
+          </>
+        )}
+      />
+    </>
   )
 }
 
