f5cd839a609d81c370daa8cdafd4c3621eaec227
Vlad Pavlovski <vladislav.pavlovski@ataccama.com>
2022-05-28 16:48:31 +0200

    Game Invitation: share email, whatsapp

--- web-react/src/admin/pages/Game/components/invitation/index.tsx
+++ web-react/src/admin/pages/Game/components/invitation/index.tsx
@@ -2,9 +2,9 @@ import { useStyles } from 'admin/pages/commonComponents/styled'
 import dayjs from 'dayjs'
 import React from 'react'
 import Img from 'react-cool-img'
-import { EmailIcon, EmailShareButton } from 'react-share'
+import { EmailIcon, EmailShareButton, WhatsappIcon, WhatsappShareButton } from 'react-share'
 import { getTeamByHost } from 'utils'
-import { Game } from 'utils/types'
+import { Game, Team } from 'utils/types'
 import AccountBoxIcon from '@mui/icons-material/AccountBox'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
@@ -212,22 +212,11 @@ const FormDialog: React.FC<TFormDialog> = props => {
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <Paper className={classes.paper}>
-                <Typography
-                  variant="subtitle2"
-                  gutterBottom
-                  sx={{ textAlign: 'center' }}
-                  component="div"
-                >
-                  Share invitation
-                </Typography>
-                <Divider />
-                <EmailShareButton
-                  url={'https://www.google.com'}
-                  subject={'Game Invitation'}
-                  body="body"
-                >
-                  <EmailIcon size={32} round />
-                </EmailShareButton>
+                <ShareGameInvite
+                  gameData={gameData}
+                  teamHost={teamHost}
+                  teamGuest={teamGuest}
+                />
               </Paper>
             </Grid>
           </Grid>
@@ -245,6 +234,70 @@ const FormDialog: React.FC<TFormDialog> = props => {
         </Button>
       </DialogActions>
     </Dialog>
+  )
+}
+
+type Props = {
+  gameData: Game
+  teamGuest: Team
+  teamHost: Team
+}
+
+const ShareGameInvite = ({ gameData, teamHost, teamGuest }: Props) => {
+  console.log(gameData)
+  const gameInviteSubject = `Game Invitation: ${gameData?.name} - ${gameData?.startDate} - ${gameData?.startTime}`
+  const gameInviteBody = `
+Game Event: ${gameData?.name} - ${dayjs(gameData?.startDate).format(
+    'LL'
+  )} ${dayjs(gameData?.startTime, 'HH:mm:ss').format('HH:mm')}
+Teams: ${teamHost?.name} vs ${teamGuest?.name}
+${gameData?.venue?.name ? 'Venue: ' : ''}${gameData?.venue?.name || ''}
+${gameData?.phase?.competition?.name ? 'Competition: ' : ''}${
+    gameData?.phase?.competition?.name || ''
+  }
+${gameData?.phase?.name ? 'Phase: ' : ''}${gameData?.phase?.name || ''}
+${gameData?.group?.name ? 'Group: ' : ''}${gameData?.group?.name || ''}
+${gameData?.foreignId ? 'Game ID: ' : ''}${gameData?.foreignId || ''}
+${gameData?.referee ? 'Referee: ' : ''}${gameData?.referee || ''}
+${gameData?.timekeeper ? 'Timekeeper: ' : ''}${gameData?.timekeeper || ''}
+`
+
+  const gameInviteUrl = ' '
+
+  const gameInviteBodyClean = gameInviteBody
+    .split('\n')
+    .filter(row => row.trim() !== '')
+    .join('\n')
+
+  return (
+    <>
+      <Typography
+        variant="subtitle2"
+        gutterBottom
+        sx={{ textAlign: 'center' }}
+        component="div"
+      >
+        Share invitation
+      </Typography>
+      <Divider />
+      <Stack
+        spacing={2}
+        direction="row"
+        justifyContent="center"
+        alignContent="center"
+      >
+        <EmailShareButton
+          url={gameInviteUrl}
+          subject={gameInviteSubject}
+          body={gameInviteBodyClean}
+        >
+          <EmailIcon size={32} round />
+        </EmailShareButton>
+        <WhatsappShareButton url={gameInviteUrl} title={gameInviteBodyClean}>
+          <WhatsappIcon size={32} round />
+        </WhatsappShareButton>
+      </Stack>
+    </>
   )
 }
 
